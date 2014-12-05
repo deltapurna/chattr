bd2ed0f7555803775c893dcb15b2c7dd13378dcf
Delta Purna Widyangga <deltapurna@gmail.com>
2014-12-05 23:49:56 +0700

    completed with bug

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-var express = require('express');
+var express       = require('express');
 var app           = express();
 var server        = require('http').createServer(app);
 var io            = require('socket.io')(server);
-var messages      = []
+var redis         = require('redis');
+var redisClient   = redis.createClient();
 
 var storeMessage  = function(name, data) {
-  messages.push({name: name, data: data});
-  if (messages.length > 10) {
-    messages.shift();
-  }
+  var message = JSON.stringify({name: name, data: data});
+  redisClient.lpush('messages', message, function(err, reply){
+    redisClient.ltrim('messages', 0, 9);
+  });
 };
 
 io.on('connection', function(client){
 
   client.on('join', function(name) {
-    messages.forEach(function(message){
-      client.emit('messages', message.name + ': ' + message.data);
+    client.broadcast.emit('add chatter', name);
+
+    redisClient.smembers('chatters', function(err, names){
+      names.forEach(function(name){
+        client.emit('add chatter', name);
+      });
     });
+
+    redisClient.sadd('chatters', name);
+
+    redisClient.lrange('messages', 0, -1, function(err, messages){
+      messages = messages.reverse();
+      messages.forEach(function(message){
+        var message = JSON.parse(message);
+        client.emit('messages', message.name + ': ' + message.data);
+      });
+    });
+
     client.nickname = name;
     console.log(client.nickname + ' connected...');
     client.broadcast.emit('chat', client.nickname + ' joined the chat');
@@ -28,6 +44,14 @@ io.on('connection', function(client){
     client.broadcast.emit('messages', nickname + ': ' + message);
     client.emit('messages', nickname + ': ' + message);
     storeMessage(nickname, message)
+  });
+
+  client.on('disconnect', function(){
+    var nickname = client.nickname;
+
+    client.broadcast.emit('remove chater', nickname);
+    console.log(nickname + ' disconnected...');
+    redisClient.srem('chatters', nickname);
   });
 });
 
