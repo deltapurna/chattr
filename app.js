b0c19e629d46489bda4e7fe70a49f6ca97542bd4
Delta Purna Widyangga <deltapurna@gmail.com>
2014-12-05 19:16:05 +0700

    store in messages array

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var express = require('express');
-var app     = express();
-var server  = require('http').createServer(app);
-var io      = require('socket.io')(server);
+var app           = express();
+var server        = require('http').createServer(app);
+var io            = require('socket.io')(server);
+var messages      = []
+
+var storeMessage  = function(name, data) {
+  messages.push({name: name, data: data});
+  if (messages.length > 10) {
+    messages.shift();
+  }
+};
 
 io.on('connection', function(client){
 
   client.on('join', function(name) {
+    messages.forEach(function(message){
+      client.emit('messages', message.name + ': ' + message.data);
+    });
     client.nickname = name;
     console.log(client.nickname + ' connected...');
+    client.broadcast.emit('chat', client.nickname + ' joined the chat');
   });
 
   client.on('messages', function(message){
@@ -15,6 +27,7 @@ io.on('connection', function(client){
 
     client.broadcast.emit('messages', nickname + ': ' + message);
     client.emit('messages', nickname + ': ' + message);
+    storeMessage(nickname, message)
   });
 });
 
